82a0762d43bc153ca3910dc7573f9836fd81037e
Dean Bensted <dean.bensted@edgeloyalty.com.au>
2018-02-23 11:33:11 +1100

    5.1

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,12 +9,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albumbInfo: Album;
+  albumInfo: Album;
 
   constructor(private _productService : ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumbInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
