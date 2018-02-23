f37ce682516a9724beeead0207c8ac320a9e45d9
Dean Bensted <dean.bensted@edgeloyalty.com.au>
2018-02-23 11:58:33 +1100

    part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
- 
+
 @Injectable()
 export class ProductService {
 
   constructor(private _http: Http) { }
 
   private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
-  public getAlbum(id: number) : Observable<Album> {
+  public getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  public getProducts() : Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
